c0d101a358454b8a4a334f5607699c1fc8204dde
danny <dannyphanth@gmail.com>
2023-07-06 10:40:28 -0700

    In ListSearchForm, implemented a table that successfully fetched and displayed data from API

--- frontend/src/Components/ListSearchForm/ListSearchForm.js
+++ frontend/src/Components/ListSearchForm/ListSearchForm.js
@@ -3,23 +3,55 @@ import axios from 'axios';
 
 function ListSearchForm() {
 
-    const { carVin, setCarVin } = useState({});
-    const { carMake, setCarMake } = useState({});
-    const { carModel, setCarModel } = useState({});
-    const { crashDate, setCrashDate } = useState({});
+    // const { carVin, setCarVin } = useState({});
+    // const { carMake, setCarMake } = useState({});
+    // const { carModel, setCarModel } = useState({});
+    // const { crashDate, setCrashDate } = useState({});
+
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        setCarVin(getCarVin());
-    })
+        //Fetch data from API
+        axios.get('http://localhost:5000/cases')
+            .then((response) => {
+                console.log("response: ", response.data);
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            });
+    }, []);
 
-    const getCarVin = () => {
-        let carVin
-    }
+
 
     return (
-        <>
 
-        </>
+        <div>
+            <h2>Data Table</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Vin</th>
+                        <th>Make</th>
+                        <th>Model</th>
+                        <th>Year</th>
+                        <th>Crash Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.car_vin}</td>
+                            <td>{item.car_make}</td>
+                            <td>{item.car_model}</td>
+                            <td>{item.car_year}</td>
+                            <td>{item.crash_date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
     )
 
 }
--- frontend/src/Views/ListSearchPage/ListSearchPage.js
+++ frontend/src/Views/ListSearchPage/ListSearchPage.js
@@ -1,10 +1,11 @@
 import Header from "../../Components/Header/Header";
-
+import ListSearchForm from "../../Components/ListSearchForm/ListSearchForm";
 function ListSearchPage() {
     return (
         <>
             <Header />
             <h1> List Search Page...</h1>
+            <ListSearchForm />
         </>
     )
 }
